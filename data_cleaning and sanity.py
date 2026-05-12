e979ff48bf2e9f2b019943c4e6599e4bec7776be
becks17hue <maulanarebecca1@gmail.com>
2026-05-12 08:45:54 +0200

    updated code

--- data_cleaning and sanity.py
+++ data_cleaning and sanity.py
@@ -8,10 +8,21 @@ print(mice_protein_expression.metadata)
 #variable information
 print(mice_protein_expression.variables)
 #basic cleaning checks
-#print(x.shape)
-#print(x.head)
-#print(x.isna().sum().head())
+print(x.shape)
+print(x.head)
+print(x.isna().sum().head())
 #added new lines
 df = x.copy()
 df = df.dropna(subset=["DYRK1A_N"])
 print(df.shape)
+#checking expected columns
+expected_columns = ["DYRK1A_N", "NR1_N"]
+for col in expected_columns:
+    if col not in df.columns:
+        raise ValueError(f"missing expected columns: {col}")
+    
+if len(df)< 500:
+    print("warning: Dataset is smaller than expected after cleaning")
+
+if df["DYRK1A_N"] .max() > 20:
+    print("warning: expression values exceed expected biological range")
